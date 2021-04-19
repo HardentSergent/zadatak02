49b73db5f49366e48c7238e8b391edd38ca510c2
Ivan Škacan <43252370+HardentSergent@users.noreply.github.com>
2021-04-19 20:31:59 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zadatak</title>
+</head>
+<body>
+    <h3>Zadatak Index</h3>
+    <p>Drugi zadatak</p>
+    <a href="about.php">About</a>
+    <a href="privacy.php">Privacy</a>
+</body>
+</html>
